273bbf2e224ea07d19e4e220680fb737dba88db4
Joao Henrique <joao.henrique87@outlook.com>
2023-04-28 10:29:21 -0300

    adiciona verificação de limite de saque

--- main.py
+++ main.py
@@ -32,16 +32,27 @@ while True:
 
     elif opcao == '2':
         print('Área de Saque')
+
+        if numero_saques >= LIMITE_SAQUES:
+            print('Você atingiu o número máximo de 3 saques diários.')
+            continue
+
         valor_saque = float(input('Informe o valor a ser sacado: R$ '))
         
-        if valor_saque > 0 and valor_saque <= saldo:
+        if valor_saque > 500:
+            print('Valor excede o limite.\nO limite para cada saque é R$ 500,00.')
+
+        elif valor_saque > 0 and valor_saque <= saldo:
             saldo -= valor_saque
+            numero_saques += 1
             extrato += f'SAQUE.............-R$ {valor_saque:.2f}\n'
             print(f'Valor de R$ {valor_saque:.2f} sacado com sucesso!')
+
         elif valor_saque > saldo:
-            print(f'Saldo insuficiente para saque.')
+            print('Saldo insuficiente para saque.')
+
         else:
-            print(f'Valor informado é igual ou menos que 0 e é inválido.')
+            print('Valor informado é igual ou menos que 0 e é inválido.')
 
     elif opcao == '3':
         print('EXTRATO BANCÁRIO\n')
